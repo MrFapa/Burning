fbe72bae207088e6fbd6b1b20e899a67bf1d62c2
Jutowa <jutowa@googlemail.com>
2022-07-01 09:18:22 +0200

    Quick and Dirty

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -6,7 +6,6 @@ public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] float velocity;
     [SerializeField] protected CollisionCheck checkGround0;
-    [SerializeField] protected CollisionCheck checkGround1;
 
     private float dircetion;
     private Rigidbody2D mRigidbody;
@@ -20,10 +19,11 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (this.checkGround0.CollionsWithTag("Ground") || this.checkGround1.CollionsWithTag("Ground"))
+        if (this.checkGround0.CollionsWithTag("Ground"))
         {
             this.dircetion *= -1;
         }
         mRigidbody.velocity = this.mRigidbody.velocity = new Vector2(this.dircetion * this.velocity, this.mRigidbody.velocity.y);
+        this.gameObject.transform.localScale = new Vector3(dircetion * -1, 1, 0);
     }
 }
