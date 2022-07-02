e6453abbe022082ad64540a8f365cdb3d94886e1
Jutowa <jutowa@googlemail.com>
2022-07-02 10:36:56 +0200

    brennende Äste

---
+++ Assets/Scripts/Environment/Branch.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Branch : MonoBehaviour
+{
+    [SerializeField] private float lifeTime;
+    [SerializeField] private int damage;
+    private float createdTime;
+
+    private void Awake()
+    {
+        this.createdTime = Time.time;
+    }
+
+    private void FixedUpdate()
+    {
+        if (createdTime + lifeTime <= Time.time)
+        {
+            GameObject.Destroy(this.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && collision.gameObject.GetComponent<IDamagable>() != null)
+        {
+            collision.gameObject.GetComponent<IDamagable>().ReceiveDamage(this.damage, this.transform.position);
+            GameObject.Destroy(this.gameObject);
+        }
+        else if (collision.gameObject.tag == "Ground") GameObject.Destroy(this.gameObject);
+    }
+
+}
---
+++ Assets/Scripts/Environment/BranchDrop.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BranchDrop : MonoBehaviour
+{
+    [SerializeField] GameObject branch;
+    [SerializeField] float length;
+    [Range(0f, 1f)][SerializeField] float frequency;
+
+    void FixedUpdate()
+    {
+        if ((int) Random.Range(0, 60 * (1 - this.frequency)) == 0)
+        {
+            GameObject tmp = Instantiate(branch);
+            tmp.transform.position = new Vector2(this.transform.position.x + this.length * Random.Range(-0.5f, 0.5f), this.transform.position.y);
+        }
+    }
+}
