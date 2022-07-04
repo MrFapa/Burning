f1c7a4d7941ac7a7a9c95650f3de3f9b2c5a739f
Jutowa <jutowa@googlemail.com>
2022-07-04 15:18:21 +0200

    Spieler erhält ShapeShift durch event

--- Assets/Scripts/Player/PlayerHandler.cs
+++ Assets/Scripts/Player/PlayerHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -19,6 +20,7 @@ public class PlayerHandler : MonoBehaviour, IDamagable
     [SerializeField] protected float monkeyMass;
     [SerializeField] protected float monkeyClimbingVelocity;
     [SerializeField] protected GameObject[] hearths;
+
     [Range(0, 0.01f)][SerializeField] protected float jaguarJumpAgility;
     [Range(0, 0.01f)][SerializeField] protected float monkeyHorizontalAgility;
 
@@ -27,6 +29,9 @@ public class PlayerHandler : MonoBehaviour, IDamagable
     private PlayerAnimationController playerAnimation;
     private Rigidbody2D playerRigibody;
     private static Shapes currentForm;
+
+    private static bool monkeyShape;
+    private static bool jaguarShape;
 
     private float lastHit;
 
@@ -39,8 +44,9 @@ public class PlayerHandler : MonoBehaviour, IDamagable
 
     private void Awake()
     {
-        currentForm = Shapes.Jaguar;
-        
+        monkeyShape = true;
+        jaguarShape = false;
+
         this.playerShapeShift = this.gameObject.GetComponent<PlayerShapeShift>();
         this.playerMovement = this.gameObject.GetComponent<PlayerMovement>();
         this.playerRigibody = this.gameObject.GetComponent<Rigidbody2D>();
@@ -49,12 +55,41 @@ public class PlayerHandler : MonoBehaviour, IDamagable
 
         this.lastHit = Time.time;
 
-        this.JaguarShift();
+        this.MonkeyShift();
     }
 
     public static Shapes GetShape()
     {
         return currentForm;
+    }
+
+    public void ReceiveDamage(int damageAmount, Vector3 pos)
+    {
+        if (Time.time >= this.lastHit + 1f)
+        {
+            lastHit = Time.time;
+            this.health -= damageAmount;
+
+            int direction = (this.GetComponent<Transform>().position.x < pos.x) ? -1 : 1;
+            this.playerRigibody.AddForce(new Vector2(direction * 400, 400));
+
+            this.hearths[this.health].SetActive(false);
+
+            if (this.health <= 0)
+            {
+                Die();
+            }
+        }
+    }
+
+    public void EnableJaguar()
+    {
+        jaguarShape = true;
+    }
+
+    public void EnableMonkey()
+    {
+        monkeyShape = true;
     }
 
     private void ShapeShift()
@@ -69,46 +104,33 @@ public class PlayerHandler : MonoBehaviour, IDamagable
         }
     }
 
-    public void ReceiveDamage(int damageAmount, Vector3 pos)
-    {
-        if (Time.time >= this.lastHit + 1f)
-        {
-            lastHit = Time.time;
-            this.health -= damageAmount;
-
-            int direction = (this.GetComponent<Transform>().position.x < pos.x ) ? -1 : 1;
-            this.playerRigibody.AddForce(new Vector2 (direction * 400, 400));
-
-            this.hearths[this.health].SetActive(false);
-
-            if (this.health <= 0)
-            {
-                Die();
-            }
-        }
-    }
-
     private void JaguarShift()
     {
-        currentForm = Shapes.Jaguar;
-        this.playerAnimation.setShape(0);
-        this.playerMovement.SetVelocity(this.jaguarVelocity);
-        this.playerMovement.SetJumpForce(this.jaguarJumpForce);
-        this.playerMovement.SetjumpHorizontalAgility(this.jaguarJumpAgility);
-        this.playerMovement.SetClimbCharacter(false);
-        this.playerRigibody.mass = this.jaguarMass;
+        if (jaguarShape)
+        {
+            currentForm = Shapes.Jaguar;
+            this.playerAnimation.setShape(0);
+            this.playerMovement.SetVelocity(this.jaguarVelocity);
+            this.playerMovement.SetJumpForce(this.jaguarJumpForce);
+            this.playerMovement.SetjumpHorizontalAgility(this.jaguarJumpAgility);
+            this.playerMovement.SetClimbCharacter(false);
+            this.playerRigibody.mass = this.jaguarMass;
+        }
     }
 
     private void MonkeyShift()
     {
-        currentForm = Shapes.Monkey;
-        this.playerAnimation.setShape(1);
-        this.playerMovement.SetVelocity(this.monkeyVelocity);
-        this.playerMovement.SetJumpForce(this.monkeyJumpForce);
-        this.playerMovement.SetClimbVelocity(this.monkeyClimbingVelocity);
-        this.playerMovement.SetjumpHorizontalAgility(this.monkeyHorizontalAgility);
-        this.playerMovement.SetClimbCharacter(true);
-        this.playerRigibody.mass = this.monkeyMass;
+        if (monkeyShape)
+        {
+            currentForm = Shapes.Monkey;
+            this.playerAnimation.setShape(1);
+            this.playerMovement.SetVelocity(this.monkeyVelocity);
+            this.playerMovement.SetJumpForce(this.monkeyJumpForce);
+            this.playerMovement.SetClimbVelocity(this.monkeyClimbingVelocity);
+            this.playerMovement.SetjumpHorizontalAgility(this.monkeyHorizontalAgility);
+            this.playerMovement.SetClimbCharacter(true);
+            this.playerRigibody.mass = this.monkeyMass;
+        }   
     }
 
     private void Die()
